6fd493731f159d1f2b64cd558b287e5780cf79c1
Difinition <vladdifinition@mail.ru>
2020-08-27 19:17:21 +0300

    Доработка правко
    - доработка слайдера
    - доработка адаптива
    - доработка кликающихся плашек в меню

--- src/js/desc-round-info.js
+++ src/js/desc-round-info.js
@@ -1,18 +1,24 @@
 let circles = [...document.querySelectorAll('[data-round]')];
 
 if (circles) circles.forEach(el => el.addEventListener('click', clickCirclesHandler));
+let timer;
 
 function clickCirclesHandler() {
-  let timer;
-
   this.querySelector('.desc__item').style.opacity = 1;
+  this.setAttribute('data-state', 'enable');
+  this.addEventListener('click', closeCirclesItem);
   this.removeEventListener('click', clickCirclesHandler);
 
   timer = setTimeout(() => {
     this.querySelector('.desc__item').style.opacity = 0;
+    this.setAttribute('data-state', 'disable');
     this.addEventListener('click', clickCirclesHandler);
   }, 10000);
 }
 
-
-
+function closeCirclesItem() {
+  clearTimeout(timer);
+  this.setAttribute('data-state', 'disable');
+  this.addEventListener('click', clickCirclesHandler);
+  this.querySelector('.desc__item').style.opacity = 0;
+}
